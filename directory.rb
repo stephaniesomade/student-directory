fa78b0e1d0475d4bd9e2058444a115aa50a71886
stephaniesomade <stephaniesomadee@gmail.com>
2022-02-25 19:20:40 +0000

    Replacing with methods

--- directory.rb
+++ directory.rb
@@ -13,9 +13,21 @@ students = [
     "Norman Bates"
 ]
 
-puts "The students of Villians Academy"
-puts "------------------"
-students.each { |student| puts student }
+def print_header
+    puts "The students of Villians Academy"
+    puts "----------------"
+end
 
-# print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print(names)
+    names.each do |name|
+        puts name
+    end
+end
+
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
