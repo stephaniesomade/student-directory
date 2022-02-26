f2397f8573a745a774caf5811c31b9b8e1a26c81
stephaniesomade <stephaniesomadee@gmail.com>
2022-02-26 20:28:11 +0000

    Saving the data to a file

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ end
 def print_menu 
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end 
 
@@ -25,6 +26,16 @@ def input_students
   end
     @students
 end 
+
+def save_students 
+  file = File.open("students.csv", "w")
+  @students.each { |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(", ")
+    file.puts csv_line
+    }
+  file.close
+end
 
 def show_students
   print_header
@@ -53,6 +64,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else 
