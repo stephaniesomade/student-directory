5c024cb31da0f0073bc880a9c9e33ee219372c38
stephaniesomade <stephaniesomadee@gmail.com>
2022-02-26 20:10:29 +0000

    Adding instance variables

--- directory.rb
+++ directory.rb
@@ -1,15 +1,17 @@
 # print the list of the students
+
+@students = []
 def input_students 
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    students = []
+    @students = []
     name = gets.chomp
     while !name.empty? do 
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
         name = gets.chomp
     end
-    students
+    @students
 end    
 
 def print_header
@@ -17,43 +19,46 @@ def print_header
     puts "----------------"
 end
 
-def print(students)
-    students.each do |student|
+def print_student_list
+    @students.each do |student|
         puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+def print_footer
+    puts "Overall, we have #{@students.count} great students"
+end
+
+def process(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students
+    when "9"
+        exit # this will cause the program to terminate
+    else 
+        puts "I don't know what you meant, try again"
+    end 
 end
 
 def interactive_menu 
-students = []
 loop do 
-# 1. print the menu and ask what the user would like to do
-puts "1. Input the students"
-puts "2. Show the students"
-puts "9. Exit"
-#2. read the input and save it to a var
-selection = gets.chomp
-# go ahead with what user asked
-case selection 
-when "1"
-    # input students
-    students = input_students
-when "2"
-    # show the students
-    print_header
-    print(students)
-    print_footer(students)
-when "9"
-    exit # this will cause the program to terminate
-else 
-    puts "I don't know what you meant, try again"
-end
+    print_menu
+    process(gets.chomp)
 end
 end 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+def print_menu 
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end 
+
+def show_students
+    print_header
+    print_student_list
+    print_footer
+end
+
+interactive_menu
