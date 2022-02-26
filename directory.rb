eceddd23c33306096543c42c58116697d2ff898c
stephaniesomade <stephaniesomadee@gmail.com>
2022-02-26 20:18:30 +0000

    Tidying up the order

--- directory.rb
+++ directory.rb
@@ -1,64 +1,63 @@
 # print the list of the students
-
 @students = []
-def input_students 
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-    @students = []
-    name = gets.chomp
-    while !name.empty? do 
-        @students << {name: name, cohort: :november}
-        puts "Now we have #{@students.count} students"
-        name = gets.chomp
-    end
-    @students
-end    
-
-def print_header
-    puts "The students of Villians Academy"
-    puts "----------------"
-end
-
-def print_student_list
-    @students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-end
-
-def print_footer
-    puts "Overall, we have #{@students.count} great students"
-end
-
-def process(selection)
-    case selection
-    when "1"
-        input_students
-    when "2"
-        show_students
-    when "9"
-        exit # this will cause the program to terminate
-    else 
-        puts "I don't know what you meant, try again"
-    end 
-end
 
 def interactive_menu 
-loop do 
+  loop do 
     print_menu
     process(gets.chomp)
+  end
 end
-end 
 
 def print_menu 
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end 
+
+def input_students 
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = gets.chomp
+  while !name.empty? do 
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
+    @students
 end 
 
 def show_students
-    print_header
-    print_student_list
-    print_footer
+  print_header
+  print_student_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villians Academy"
+  puts "----------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else 
+    puts "I don't know what you meant, try again"
+  end 
 end
 
 interactive_menu
